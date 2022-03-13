144a200d70a8d15cd23882341231cacca9b338e4
Luiz Bruno <luiz_seya@hotmail.com>
2022-03-13 02:37:43 -0300

    Ajustando a movimentação

--- Infinity Runner/Assets/Scripts/Player.cs
+++ Infinity Runner/Assets/Scripts/Player.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rig;
+
+    public float speed;
+
+    public float jumpforce;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() //É chamado pela física da unity
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+        }
     }
 }
