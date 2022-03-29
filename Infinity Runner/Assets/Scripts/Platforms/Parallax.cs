cdef82cbf143272b3951e8328f41907cbb624348
Luiz Bruno <luiz_seya@hotmail.com>
2022-03-29 11:31:50 -0300

    Parallax

--- Infinity Runner/Assets/Scripts/Platforms/Parallax.cs
+++ Infinity Runner/Assets/Scripts/Platforms/Parallax.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    public float length;
+    public float startPos;
+
+    public Transform cam;
+
+    public float parallaxFactor;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        float reposition = cam.transform.position.x * (1 - parallaxFactor);
+        float distance = cam.transform.position.x * parallaxFactor;
+
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+
+        if(reposition > startPos + length)
+        {
+            startPos += length;
+        }
     }
 }
